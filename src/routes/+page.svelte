42073aa6cebb06c83e018b0b70956ce5aea0de68
Muhammad Saheed <muhammad.saheed.iam@gmail.com>
2023-02-06 20:37:49 +0530

    Update description metadata

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="PhonLit, a phonetic transliteration tool" />
 </svelte:head>
 
 <section>
